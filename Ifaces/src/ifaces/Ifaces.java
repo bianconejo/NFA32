aab2619f5d8aed06a353f489f8587c66eb7f7aef
roletar <roletar@IQ-C1-02.iut21.u-bourgogne.fr>
2013-06-04 19:07:08 +0200

    TD 10 interfaces

---
+++ Ifaces/src/ifaces/Ifaces.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifaces;
+
+/**
+ *
+ * @author roletar
+ */
+public class Ifaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
